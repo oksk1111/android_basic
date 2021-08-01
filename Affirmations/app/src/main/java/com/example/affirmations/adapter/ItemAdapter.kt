a6838c62f03f5edcce7b7892187722004eefa080
oksk1111 <oksk1111@gmail.com>
2021-08-01 12:57:52 +0900

    Android_kotlin_basic_Affirmation_MDC

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -22,6 +23,7 @@ class ItemAdapter(
     // You provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.item_image)
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
@@ -39,6 +41,7 @@ class ItemAdapter(
      */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
+        holder.imageView.setImageResource(item.imageResourceId)
         holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 
